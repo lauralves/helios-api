8b3f74b7d0e15b4c123a8535b7bdc6fe21818fd1
Laura Alves <laura-alves1@live.com>
2026-07-18 19:20:38 -0300

    initial configs

---
+++ src/main/java/io/github/lauralves/helios_api/HeliosApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.lauralves.helios_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lauralves/helios_api/HeliosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lauralves.helios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
